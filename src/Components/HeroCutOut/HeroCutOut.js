6891bf56b5182f6bc6be9ba45c6534b390456cf6
unknown <miabubacker98@gmail.com>
2023-10-20 18:17:45 +0530

    css changes

--- src/Components/HeroCutOut/HeroCutOut.js
+++ src/Components/HeroCutOut/HeroCutOut.js
@@ -3,11 +3,11 @@ import "./HeroCutOut.scss";
 
 export default function HeroCutOut() {
   return (
-    <div className="heroCutOut_Conatiner">
-      <section className="titleConatiner">
-     <h1>do it <span>right</span></h1>
-      </section>
-      <div>heroIMG</div>
-    </div>
+    <section >
+         <div className="heroText">
+         <h1>do it <span>right</span></h1>
+         </div>
+
+    </section>
   );
 }
