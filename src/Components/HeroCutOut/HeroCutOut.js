0853c085bf2ed52020ed27e37eee8709933f5f91
unknown <miabubacker98@gmail.com>
2023-10-20 16:31:32 +0530

    home and hero page

---
+++ src/Components/HeroCutOut/HeroCutOut.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeroCutOut.scss";
+
+export default function HeroCutOut() {
+  return (
+    <div className="heroCutOut_Conatiner">
+      <section className="titleConatiner">
+     <h1>do it <span>right</span></h1>
+      </section>
+      <div>heroIMG</div>
+    </div>
+  );
+}
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
+import Header from "../Header/Header";
+import HeroCutOut from "../HeroCutOut";
 
 export default function Home(props) {
   const { loading, homeActions } = props;
   return (
     <React.Fragment>
-      {/* <Header {...props}/> */}
-      Home   
+      <Header {...props}/>
+      <HeroCutOut {...props}/>
+      {/* Home   
       <button
         onClick={() => {
           homeActions.fetchRequestedFeatures();
         }}
       >
         loading
-      </button>
+      </button> */}
       {/* <Routes>
             <Route path='/' element={<Dashboard {...props}/>}/>
             <Route path='product/:id' element={<Product {...props}/>}/>
