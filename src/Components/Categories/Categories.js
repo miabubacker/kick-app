084ddd411250ac0b77200070b198bc2e15bb96a1
unknown <miabubacker98@gmail.com>
2023-10-29 19:27:24 +0530

    scroll of categories

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -9,54 +9,49 @@ export default function Categories() {
      const shoes=[
         {img:shoe1,about:'Lifestyle Shoes'},
         {img:shoe2,about:'Basketball Shoes'},
-        {img:shoe1,about:'Lifestyle Shoes'},
-        {img:shoe2,about:'Basketball Shoes'},
+        {img:shoe1,about:'Lifestyles Shoes'},
+        {img:shoe2,about:'Basketballs Shoes'},
     ]
      const sliderRef= useRef(null)
       const[lengthimg,setLengthIMg]=useState(0)
       const scrollAmount=650;
       const scrollShoes=(moves)=>{
          if(moves==='left'){
-            if(window.innerWidth>=650){
+            if(window.innerWidth>=1300){
             const container = sliderRef.current;
             container.scrollLeft -= scrollAmount;
-             setLengthIMg(container.scrollLeft -= scrollAmount)
+             setLengthIMg(container.scrollLeft -= container.offsetTop)
             }
             else{
                 const container = sliderRef.current;
-                
-                container.scrollLeft -= 100;
-                 setLengthIMg(container.scrollLeft -= window.innerWidth) 
+                 setLengthIMg(container.scrollTop -= (container.offsetTop)) 
             }
          }
           else{
-            if(window.innerWidth>=650){
+            if(window.innerWidth>=1300){
             const container = sliderRef.current;
             container.scrollLeft += scrollAmount;
-            setLengthIMg(container.scrollLeft += scrollAmount)
+            setLengthIMg(container.scrollLeft += container.offsetTop)
             }
             else{
                 const container = sliderRef.current;
                  console.log(container)
                 // container.scrollLeft += container;
-                 setLengthIMg( container.scrollLeft += window.innerWidth) 
+                 setLengthIMg( container.scrollTop +=container.offsetTop) 
             }
           }
          
-      }
-    console.log(lengthimg)
-    //   const  imgContainer= window.innerWidth >= 650 ? scrollShoes() : mobilescroll();
-    
+      } 
   return (
     <>
     <section className='categoriesContainer'>
 <div className='header'>
 <div className='categoriesTitle'>Categories</div>
 <div className='buttonContainer'>
-    <button onClick={()=>{scrollShoes('left')}}  disabled={lengthimg===0|| lengthimg===-48} className={lengthimg===0|| lengthimg===-48?'disabled':'view'}>
+    <button disabled={lengthimg<=0}  className={lengthimg<=0?'disabled':'view'}onClick={()=>{scrollShoes('left')}}>
         <img  style={{transform:'rotate(-180deg'}}src={buttonLeft} />
         </button>
-    <button disabled={lengthimg===1300} className={lengthimg===1300?'disabled':'view'} onClick={()=>{scrollShoes('right')}}><img src={buttonRight}/></button>
+    <button  disabled={lengthimg>=2256|| lengthimg===1367} className={ lengthimg>=2256||lengthimg===1367?'disabled':"view"} onClick={()=>{scrollShoes('right')}}><img src={buttonRight}/></button>
     </div>
 </div>
 <div className='ShoesContainer' ref={sliderRef}>
